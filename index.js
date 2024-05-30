4ebbcef73a9a734dd4ae3dce5c332f80dd28b145
Chetopsa <125150503+Chetopsa@users.noreply.github.com>
2024-05-29 17:30:22 -0700

    varous changes made, mainly view receipts working

--- index.js
+++ index.js
@@ -35,10 +35,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // view engine
 
-
-
-
-
 // use express-session
 // in mremory session is sufficient for this assignment
 app.use(session({
@@ -50,7 +46,7 @@ app.use(session({
 // add safety for all api calls, make sure user is logged in
 function redirect2Login (requ, resp) {
   if (!(requ.session && requ.session.isLoggedIn)) {
-    resp.redirect('/login.html');
+    resp.redirect('/login');
     return true;
   }
   return false;
@@ -106,12 +102,12 @@ app.post('/api/logout', (req, res) => {
 // function to handle login form post request
 app.post('/api/loginForm', async (req, res) => {
       const { username, password } = req.body;
-      email = username; // kinda dumb whatever
+      let email = username; // kinda dumb whatever
       const query = 'SELECT * FROM Users WHERE email = $1';
       const params = [email];
       console.log(email);
       console.log(password);
-      client = await pool.connect();
+      let client = await pool.connect();
       // TODO: need to query for group_id and set upon login
      try {
         const result = await client.query(query, params);
@@ -164,7 +160,7 @@ app.post('/api/receiptEntry', async (req, res) => {
   if (group === -1) {
     return res.sendStatus(409);
   }
-  client = await pool.connect();
+  let client = await pool.connect();
   // console.log(user);
   // enter receipt entry
   console.log(group);
@@ -192,7 +188,7 @@ app.post('/api/createGroup', async (req, res) => {
   if (redirect2Login (req, res)) return;
   let user = req.session.user_id;
   let data = req.body;
-  client = await pool.connect();
+  let client = await pool.connect();
   try {
     const res1 = await client.query('SELECT * FROM Groups WHERE group_name = $1 AND group_code = $2', [data.groupName, data.groupCode]);
     //make sure group name and code doesn't already exist
@@ -215,10 +211,113 @@ app.post('/api/createGroup', async (req, res) => {
     console.log("creating group didn't work " + error);
   } finally {
     client.release();
-    return res.sendStatus(200);
+  }
+  return res.sendStatus(200);
+
+});
+// {group-name: str, group-code: str}
+app.post('/api/joinGroup', async (req, res) => {
+  if (redirect2Login (req, res)) return;
+  let user = req.session.user_id;
+  let data = req.body;
+  let client = await pool.connect();
+  try {
+    const res1 = await client.query('SELECT group_id, group_code FROM Groups WHERE group_name = $1 AND group_code = $2', [data.groupName, data.groupCode]);
+    // make sure group name and code already exist
+    if (res1.rows.length === 0) {
+      return res.sendStatus(409);
+    }
+    let group_id = res1.rows[0].group_id;
+    let group_code = res1.rows[0].group_id;
+    // make sure user isn't already apart of the group
+    const res2 = await client.query('SELECT * FROM Users_Groups WHERE group_id = $1', [group_id]);
+    if (res2.rows.length != 0) {
+      return res.sendStatus(409);
+    }
+    // create row in users-groups table
+    const res3 = await client.query('INSERT INTO Users_Groups (user_id, group_id) VALUES ($1, $2)',
+      [user, group_id]
+    );
+    console.log(group_id)
+    // add group_id to user-groups tap
+    req.session.group_id = group_id; //set session group_id to be the newly joined group
+    await client.query('COMMIT');
+  } catch (error) {
+    await client.query('ROLLBACK'); // rollback if any error occurs
+    console.log("joining group didn't work " + error);
+  } finally {
+    client.release();
+  }
+  return res.sendStatus(200);
+
+});
+
+// api for changing current group user is asscoiated with
+app.post('/api/changeGroup', async (req, res) => {
+  if (redirect2Login (req, res)) return;
+  let {group_id} = req.body;
+  req.session.group_id = group_id;
+  res.sendStatus(200);
+});
+// populate Receits page
+app.get('/api/populateReceipts', async function(req, res) {
+  if (redirect2Login (req, res)) return;
+  let user = req.session.user_id;
+  let group = req.session.group_id;
+  let receipt_list = new Array()
+  let client;
+  res.setHeader('Content-Type', 'application/json');
+  try {
+    client = await pool.connect();
+    const res1 = await client.query('SELECT group_name FROM Groups WHERE group_id = $1', [group]);
+    let group_name = res1.rows[0].group_name;
+    // get receipt info used group as key
+    const receipt_ids = await client.query('SELECT receipt_id, name FROM RECEIPTS WHERE group_id = $1', [group]);
+    receipt_ids.rows.forEach(async (id) => {
+      // use receipt key to get list of items
+      const res3 = await client.query('SELECT * FROM Items WHERE receipt_id = $1', [id.receipt_id]);
+      receipt_list.push({'id': id.receipt_id, 'receipt': id.name, 'items': res3.rows});
+    });
+    await client.query('COMMIT');
+    data = {'receipt_list': receipt_list, 'group_name': group_name};
+    console.log(data);
+    res.status(200).json(data);
+  } catch (error) {
+    await client.query('ROLLBACK');
+    res.sendStatus(500);
+    console.log(error);
+  } finally {
+    client.release();
   }
 
 });
+app.get('/api/populateGroups', async function(req, res) {
+  if (redirect2Login (req, res)) return;
+  let user = req.session.user_id;
+  let group_list = new Array();
+  let client;
+  res.setHeader('Content-Type', 'application/json');
+  try {
+    client = await pool.connect();
+    const group_ids = await client.query('SELECT group_id FROM Users_Groups WHERE user_id = $1', [user]);
+    console.log(group_ids.rows);
+    group_ids.rows.forEach(async (id) => {
+      console.log(id);
+      const group_name = await client.query('SELECT group_name FROM Groups WHERE group_id = $1', [id.group_id]);
+      group_list.push({'group_id': id.group_id, 'group_name': group_name.rows[0].group_name});
+    });
+    await client.query('COMMIT');
+    
+    let data = {'data': group_list, 'current': req.session.group_id};
+    console.log(data);
+    res.send(JSON.stringify(data));
+  } catch (error) {
+    await client.query('ROLLBACK'); // rollback if any error occurs
+    res.status(500).send('An error occurred');
+  } finally {
+    client.release();
+  }
+}); 
 
 
 // all the static file functions
@@ -226,46 +325,49 @@ app.get('/',function(req, res) {
     res.sendFile(path.join(__dirname, 'static', 'welcome.html'));
 });
 
-app.get('/createAccount.html', function(req, res) {
+app.get('/createAccount', function(req, res) {
     if (req.session && req.session.isLoggedIn) {
-      res.redirect('/home.html');
+      res.redirect('/home');
     } else {
       res.sendFile(path.join(__dirname, 'static', 'createAccount.html'));
     }
   });
-app.get('/login.html', function(req, res) {
+app.get('/login', function(req, res) {
   if (req.session && req.session.isLoggedIn) {
-    res.redirect('/home.html');
+    res.redirect('/home');
   } else {
     res.sendFile(path.join(__dirname, 'static', 'login.html'));
   }
 });
-app.get('/home.html', function(req, res) {
+app.get('/home', function(req, res) {
   if (req.session && req.session.isLoggedIn) {
     res.sendFile(path.join(__dirname, 'static', 'home.html'));
   } else {
-    res.redirect('/login.html');
+    res.redirect('/login');
   }
 });
-app.get('/manageGroups.html', function(req, res) {
+app.get('/manageGroups', function(req, res) {
   if (req.session && req.session.isLoggedIn) {
     res.sendFile(path.join(__dirname, 'static', 'manageGroups.html'));
   } else {
-    res.redirect('/login.html');
+    res.redirect('/login');
   }
 });
 app.get('/addReceipt', function(req, res) {
   if (req.session && req.session.isLoggedIn) {
-    res.sendFile(path.join(__dirname, 'public', 'addReceipt'));
+    res.sendFile(path.join(__dirname, 'static', 'addReceipt.html'));
   } else {
-    res.redirect('/login.html');
+    res.redirect('/login');
   }
+});
+app.get('/viewReceipts', function(req, res) {
+  if (redirect2Login (req, res)) return;
+  res.sendFile(path.join(__dirname, 'static', 'viewReceipts.html'));
 });
 // middle ware to serve static files
 app.get('/static/*', function(req, res) {
-  if (req.session && req.session.isLoggedIn) {
-    app.use('/static', express.static(__dirname + '/static'));
-  }
+  if (redirect2Login (req, res)) return;
+  res.sendFile(path.join(__dirname, 'static', 'addReceipt.html'));
 });
 
 // function to return the 404 message and error to client
